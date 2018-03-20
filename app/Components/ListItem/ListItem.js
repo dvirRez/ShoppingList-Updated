5a00d583f440d79f23407a14b5aeb919d272d1a1
Dvir Rezenman <dvir.rez@gmail.com>
2018-03-20 17:53:28 +0200

    Added mark product logic

--- app/Components/ListItem/ListItem.js
+++ app/Components/ListItem/ListItem.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { basicItem, deletedItem } from './styles.css';
 
-export default function ListItem({ handleChange, removeItem, title, itemId }) {
-    return (
-        <div>
-            <input
-                type="checkbox"
-                onChange={handleChange} />
-            <span>{title}</span>
-            <span onClick={removeItem}>X</span>
-        </div>
-    );
+export default class ListItem extends React.Component {
+
+    static propTypes = {
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        removeItem: PropTypes.func.isRequired,
+    };
+
+    state = {
+        checkBoxClassName: basicItem,
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            checkBoxClassName: e.target.checked ? deletedItem : basicItem,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <input
+                    type="checkbox"
+                    onChange={this.handleChange} />
+                <span className={this.state.checkBoxClassName} >{this.props.title}</span>
+                <span onClick={this.props.removeItem}>X</span>
+            </div>
+        );
+    }
+
 }
